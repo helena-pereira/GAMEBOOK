3c3deb97a3d59524759770d2ef281cdcff5e19ed
Stéphanie Pinto <stephanie.pinto1@students.hevs.ch>
2016-11-26 13:46:13 +0100

    2

--- app/src/main/java/com/example/helena/gamebook/TheMatch.java
+++ app/src/main/java/com/example/helena/gamebook/TheMatch.java
@@ -1,9 +1,11 @@
 package com.example.helena.gamebook;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -68,6 +70,38 @@ public class TheMatch extends AppCompatActivity {
     public void toListMatch(View view) {
         Intent toListMatch = new Intent(this,MatchList.class);
         startActivity(toListMatch);
+    }
+
+
+    public void toDelete(View view){
+    AlertDialog.Builder alertDeleteMatch = new AlertDialog.Builder(this);
+    // Setting Dialog Title
+    alertDeleteMatch.setTitle(R.string.deleteMatchTitle);
+
+    // Setting Dialog Message
+    alertDeleteMatch.setMessage(R.string.deleteMatchMessage);
+
+    // Setting Icon to Dialog
+    alertDeleteMatch.setIcon(R.mipmap.delete);
+
+    // Setting Positive "Yes" Button
+    alertDeleteMatch.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog,int which) {
+
+            // Write your code here to invoke YES event
+            dialog.cancel();
+        }
+    });
+
+    // Setting Negative "NO" Button
+    alertDeleteMatch.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+        public void onClick(DialogInterface dialog, int which) {
+            // Write your code here to invoke NO event
+            dialog.cancel();
+        }
+    });
+    // Showing Alert Message
+    alertDeleteMatch.show();
     }
 
     private void updateViews() {
