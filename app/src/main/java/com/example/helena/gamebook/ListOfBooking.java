b7814b5a9d583afb89fcd67bda35d1d12eeb3f4f
Stéphanie Pinto <stephanie.pinto1@students.hevs.ch>
2016-11-28 19:28:09 +0100

    Yes

--- app/src/main/java/com/example/helena/gamebook/ListOfBooking.java
+++ app/src/main/java/com/example/helena/gamebook/ListOfBooking.java
@@ -1,8 +1,10 @@
 package com.example.helena.gamebook;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -83,18 +85,12 @@ public class ListOfBooking extends AppCompatActivity {
         }else{
             idCustomer = (int) savedInstanceState.getSerializable("idCustomer");
         }
-
-
-
-
-
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_search,menu);
+        inflater.inflate(R.menu.menu_basic,menu);
         return super.onCreateOptionsMenu(menu);
-
     }
 
     public void backToHome(View view){
@@ -108,41 +104,54 @@ public class ListOfBooking extends AppCompatActivity {
     }
 
 
-    //*MErci de faire le nécessaire concernant les update et
-    //les traductions
+    // refresh pour le changement de langue ou redirection pour la déconnexion
     public boolean onOptionsItemSelected(MenuItem item)
     {
         switch(item.getItemId()) {
+            case R.id.logout:
+                AlertDialog.Builder alertDeleteBooking = new AlertDialog.Builder(this);
+                // Le titre du Dialog Alert
+                alertDeleteBooking.setTitle(R.string.LogOutUserTitle);
+
+                // Message du Dialog Alert
+                alertDeleteBooking.setMessage(R.string.LogOutUserMessage);
+
+                // Icon de suppression
+                alertDeleteBooking.setIcon(R.mipmap.logout);
+
+                // si on clique oui
+                alertDeleteBooking.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int which) {
+
+                        Intent toMain = new Intent(ListOfBooking.this, MainActivity.class);
+                        toMain.putExtra("idCustomer", idCustomer);
+                        startActivity(toMain);
+                    }
+                });
+
+                // Si on clique sur non
+                alertDeleteBooking.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // ça ferme tout simplement le dialog
+                        dialog.cancel();
+                    }
+                });
+                // On montre l'alerte
+                alertDeleteBooking.show();
+                break;
             case R.id.id_enFlag:
-                LocaleHelper.setLocale(this,"en");
-                updateViews();
+                LocaleHelper.setLocale(this, "en");
+                Intent toTheSame = new Intent(ListOfBooking.this, ListOfBooking.class);
+                toTheSame.putExtra("idCustomer", idCustomer);
+                startActivity(toTheSame);
                 break;
             case R.id.id_frFlag:
-                LocaleHelper.setLocale(this,"fr");
-                updateViews();
-                break;
-            default:
-                LocaleHelper.setLocale(this,"en");
-                updateViews();
+                LocaleHelper.setLocale(this, "fr");
+                toTheSame = new Intent(ListOfBooking.this, ListOfBooking.class);
+                toTheSame.putExtra("idCustomer", idCustomer);
+                startActivity(toTheSame);
                 break;
         }
-        return true;
+        return false;
     }
-
-
-    private void updateViews() {
-        Resources resources = getResources();
-
-        ImageButton football = (ImageButton)findViewById(R.id.imageButtonFootball);
-        ImageButton booking = (ImageButton)findViewById(R.id.imageButtonBooking);
-        ImageButton user = (ImageButton)findViewById(R.id.imageButtonUser);
-
-        football.setContentDescription(resources.getString(R.string.idfootball));
-        booking.setContentDescription(resources.getString(R.string.idBooking));
-        user.setContentDescription(resources.getString(R.string.idUser));
-    }
-
-
-
-
 }
