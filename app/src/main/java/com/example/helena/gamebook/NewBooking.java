b81b8e9794f11251eec3343ac6ed3ca788db31df
Stéphanie Pinto <stephanie.pinto1@students.hevs.ch>
2016-11-28 21:16:00 +0100

    Yes

--- app/src/main/java/com/example/helena/gamebook/NewBooking.java
+++ app/src/main/java/com/example/helena/gamebook/NewBooking.java
@@ -1,10 +1,12 @@
 package com.example.helena.gamebook;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -107,23 +109,55 @@ public class NewBooking extends AppCompatActivity {
 
     }
 
+    // refresh pour le changement de langue ou redirection pour la déconnexion
     public boolean onOptionsItemSelected(MenuItem item)
     {
         switch(item.getItemId()) {
+            case R.id.logout:
+                AlertDialog.Builder alertDeleteBooking = new AlertDialog.Builder(this);
+                // Le titre du Dialog Alert
+                alertDeleteBooking.setTitle(R.string.LogOutUserTitle);
+
+                // Message du Dialog Alert
+                alertDeleteBooking.setMessage(R.string.LogOutUserMessage);
+
+                // Icon de suppression
+                alertDeleteBooking.setIcon(R.mipmap.logout);
+
+                // si on clique oui
+                alertDeleteBooking.setPositiveButton(R.string.yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,int which) {
+
+                        Intent toMain = new Intent(NewBooking.this, MainActivity.class);
+                        toMain.putExtra("idCustomer", idCustomer);
+                        startActivity(toMain);
+                    }
+                });
+
+                // Si on clique sur non
+                alertDeleteBooking.setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        // ça ferme tout simplement le dialog
+                        dialog.cancel();
+                    }
+                });
+                // On montre l'alerte
+                alertDeleteBooking.show();
+                break;
             case R.id.id_enFlag:
-                LocaleHelper.setLocale(this,"en");
-                updateViews();
+                LocaleHelper.setLocale(this, "en");
+                Intent toTheSame = new Intent(NewBooking.this, NewBooking.class);
+                toTheSame.putExtra("idCustomer", idCustomer);
+                startActivity(toTheSame);
                 break;
             case R.id.id_frFlag:
-                LocaleHelper.setLocale(this,"fr");
-                updateViews();
-                break;
-            default:
-                LocaleHelper.setLocale(this,"en");
-                updateViews();
+                LocaleHelper.setLocale(this, "fr");
+                toTheSame = new Intent(NewBooking.this, NewBooking.class);
+                toTheSame.putExtra("idCustomer", idCustomer);
+                startActivity(toTheSame);
                 break;
         }
-        return true;
+        return false;
     }
 
     public void toTheMatch(View view) {
@@ -148,10 +182,5 @@ public class NewBooking extends AppCompatActivity {
         toTheMatch.putExtra("idCustomer", idCustomer);
         //toTheMatch.putExtra("idBooking", idBooking1);
         startActivity(toTheMatch);
-    }
-
-    private void updateViews() {
-        Resources resources = getResources();
-
     }
 }
